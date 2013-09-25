42de344869bf9f586b125f00e8cd0f2d1bf4e667
jmpc <jmpc@rain>
2013-09-24 23:57:25 -0300

    Implementamos el plugin manager

--- BizativitiProject/src/com/fing/pis/bizativiti/core/PluginManager.java
+++ BizativitiProject/src/com/fing/pis/bizativiti/core/PluginManager.java
@@ -1,5 +1,58 @@
 package com.fing.pis.bizativiti.core;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fing.pis.bizativiti.core.common.IPlugin;
+
 public class PluginManager {
+
+    private Map<String, IPlugin> plugins = new HashMap<String, IPlugin>();
+
+    /**
+     * Registra un plugin en el sistema
+     * 
+     * @param plugin
+     * @throws IllegalArgumentException
+     *             si ya existe un plugin registrado que maneja el mismo tipo de
+     *             archivo
+     */
+    public void register(IPlugin plugin) {
+        if (plugin == null) {
+            throw new NullPointerException("null plugin isn't valid");
+        }
+        IPlugin oldPlugin = plugins.get(plugin.getType());
+        if (oldPlugin != null) {
+            throw new IllegalArgumentException("Plugin " + oldPlugin.getClass()
+                    + " already registered for handle type " + plugin.getType());
+        }
+        plugins.put(plugin.getType(), plugin);
+    }
+
+    /**
+     * Obtiene el plugin correspondiente al tipo que se encuentra en el sistema
+     * 
+     * @param plugin
+     * @throws IllegalArgumentException
+     *             si no hay un plugin registrado para el tipo dado
+     */
+    public IPlugin getPlugin(String type) {
+        IPlugin plugin = plugins.get(type);
+        if (plugin == null) {
+            throw new IllegalArgumentException("No plugin registered for type " + type);
+        }
+        return plugin;
+    }
+
+    /**
+     * Registra los plugins referenciados en el flujo de entrada. El flujo de
+     * entrada debe contener el nombre de la clase que implementa IPlugin
+     * 
+     * @param is
+     */
+    public void scan(InputStream is) {
+        // TODO
+    }
 
 }
