3da6627d6545f8351f0607e1f9983c09f90e093b
jmpc <jmpc@rain>
2013-09-25 00:37:01 -0300

    Agregamos archivo Main al core

---
+++ BizativitiProject/src/com/fing/pis/bizativiti/core/Main.java
@@ -0,0 +1,78 @@
+package com.fing.pis.bizativiti.core;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import com.fing.pis.bizativiti.core.common.IPlugin;
+import com.fing.pis.bizativiti.core.common.metamodel.MetamodelElement;
+
+public class Main {
+
+    /**
+     * Método principal que se encarga de convertir el flujo de entrada en BPMN.
+     * 
+     * @param plugins
+     *            flujo que contiene las indicaciones de los plugins a cargar.
+     * @param in
+     *            flujo de entrada
+     * @param type
+     *            tipo de plugin a usarse para la conversión
+     * @param out
+     *            flujo de salida en BPMN
+     */
+    public void convert(InputStream plugins, InputStream in, String type, OutputStream out) {
+        PluginManager pm = new PluginManager();
+        pm.scan(plugins);
+        IPlugin plugin = pm.getPlugin(type);
+        List<MetamodelElement> elements = plugin.parse(in);
+        // TODO: convertir utilizando Factory de BPMN
+    }
+
+    /**
+     * Punto de entrada para el cliente de linea de comandos. Ejemplo:
+     * archivo_plugins tipo_plugin archivo_entrada archivo_salida
+     * 
+     * @param args
+     */
+    // FIXME: mejorar manejo de errores y parametros a usar
+    public static void main(String[] args) {
+        InputStream pluginsStream;
+        String type;
+        InputStream inStream;
+        OutputStream outStream;
+        try {
+            pluginsStream = new FileInputStream(args[0]);
+            type = args[1];
+            inStream = new FileInputStream(args[2]);
+            outStream = new FileOutputStream(args[3]);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Main main = new Main();
+        main.convert(pluginsStream, inStream, type, outStream);
+        try {
+            pluginsStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            inStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            outStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
