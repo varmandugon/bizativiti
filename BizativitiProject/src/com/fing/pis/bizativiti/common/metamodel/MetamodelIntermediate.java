3ff5f27116c5a171dc7160e0d67669fcfb92e82e
fabianarol <fabianarol05@gmail.com>
2013-09-25 23:16:05 -0300

    Se suben los metamodel de los eventos intermedios (me qda una duda con respecto al atributo trigger, si va y donde)

---
+++ BizativitiProject/src/com/fing/pis/bizativiti/common/metamodel/MetamodelIntermediate.java
@@ -0,0 +1,10 @@
+package com.fing.pis.bizativiti.common.metamodel;
+
+public class MetamodelIntermediate extends MetamodelEvent {
+
+    public MetamodelIntermediate(String id, String name, String description, double x, double y, double width,
+            double height, String lane) {
+        super(id, name, description, x, y, width, height, lane);
+    }
+
+}
---
+++ BizativitiProject/src/com/fing/pis/bizativiti/common/metamodel/MetamodelIntermediateMessageCatch.java
@@ -0,0 +1,10 @@
+package com.fing.pis.bizativiti.common.metamodel;
+
+public class MetamodelIntermediateMessageCatch extends MetamodelIntermediate {
+
+    public MetamodelIntermediateMessageCatch(String id, String name, String description, double x, double y,
+            double width, double height, String lane) {
+        super(id, name, description, x, y, width, height, lane);
+    }
+
+}
