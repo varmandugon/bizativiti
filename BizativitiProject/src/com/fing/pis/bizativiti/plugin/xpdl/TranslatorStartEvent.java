2be984c282f7975fa8cc1cdcbf5e8ebc27c6200f
tam-09 <tam.th09@gmail.com>
2013-09-28 00:11:35 -0300

    Nueva clase que agrupa los translator startEvent, MessageStartEvent y TimerStartEvent. Falta implementar la obtension de coordenadas del nodegraphics

---
+++ BizativitiProject/src/com/fing/pis/bizativiti/plugin/xpdl/TranslatorStartEvent.java
@@ -0,0 +1,52 @@
+package com.fing.pis.bizativiti.plugin.xpdl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fing.pis.bizativiti.common.metamodel.MetamodelElement;
+import com.fing.pis.bizativiti.common.metamodel.MetamodelMessageStartEvent;
+import com.fing.pis.bizativiti.common.metamodel.MetamodelStartEvent;
+import com.fing.pis.bizativiti.common.metamodel.MetamodelTimerStartEvent;
+
+public class TranslatorStartEvent extends ATranslator {
+
+    @Override
+    public List<MetamodelElement> translate(Converter f, Object node, List<Object> pathFromRoot) {
+        org.wfmc._2009.xpdl2.StartEvent event = (org.wfmc._2009.xpdl2.StartEvent) node;
+        org.wfmc._2009.xpdl2.Activity parent = (org.wfmc._2009.xpdl2.Activity) pathFromRoot
+                .get(pathFromRoot.size() - 3);
+        // obtenemos los elementos necesarios para el evento
+        String id = Util.getId(parent);
+        String name = Util.getName(parent);
+        String description = Util.getDescription(parent);
+        double x = 0; // TODO
+        double y = 0; // TODO
+        double width = 0; // TODO
+        double height = 0; // TODO
+        String lane = ""; // TODO
+
+        List<MetamodelElement> result = new ArrayList<MetamodelElement>();
+        // vemos que tipo de evento inicio estamos tratando
+        String sTrigger = event.getTrigger();
+        if (sTrigger == "None") {
+            MetamodelStartEvent itemStartEvent = new MetamodelStartEvent(id, name, description, x, y, width, height,
+                    lane, sTrigger);
+            result.add(itemStartEvent);
+        } else if (sTrigger == "Timer") {
+					String triggerType = Util.getTriggerType(event);
+					String sTriggerAttr = Util.getTriggerAttrTimer(event);
+					MetamodelTimerStartEvent itemTimerStart = new MetamodelTimerStartEvent(id, name, description, x, y, width,
+							height, lane, sTrigger, triggerType, sTriggerAttr);
+					result.add(itemTimerStart);
+        } else if (sTrigger == "Message") {
+					String sMessageId = Util.getMessageId(event);
+					MetamodelMessageStartEvent itemMessageStart = new MetamodelMessageStartEvent(id, name, description, x, y,
+							width, height, lane, sTrigger, sMessageId);
+					result.add(itemMessageStart);
+        } else {
+            throw new RuntimeException("No Events inside StartEvents tag");
+        }
+
+        return result;
+    }
+}
