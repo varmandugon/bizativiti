34d58b852c0d70e418d68f7ff8f1d964c50e52df
varmandugon <victoria.armandugon@gmail.com>
2013-09-28 09:36:19 -0300

    Se agregan los MetamodelGateway en el modulo Commons

---
+++ BizativitiProject/src/com/fing/pis/bizativiti/common/metamodel/MetamodelEventBasedGateway.java
@@ -0,0 +1,10 @@
+package com.fing.pis.bizativiti.common.metamodel;
+
+public class MetamodelEventBasedGateway extends MetamodelGateway {
+
+    public MetamodelEventBasedGateway(String id, String name, String description, double x, double y, double width,
+            double height) {
+        super(id, name, description, x, y, width, height);
+    }
+
+}
