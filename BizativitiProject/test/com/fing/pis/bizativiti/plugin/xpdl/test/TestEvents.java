9adf19d7ccfa6e736723017a5c29d4a273f383f4
tam-09 <tam.th09@gmail.com>
2013-09-28 03:02:02 -0300

    Agregado de nodeGraphics

--- BizativitiProject/test/com/fing/pis/bizativiti/plugin/xpdl/test/TestEvents.java
+++ BizativitiProject/test/com/fing/pis/bizativiti/plugin/xpdl/test/TestEvents.java
@@ -7,9 +7,13 @@ import java.util.List;
 
 import org.junit.Test;
 import org.wfmc._2009.xpdl2.Activity;
+import org.wfmc._2009.xpdl2.Coordinates;
 import org.wfmc._2009.xpdl2.Description;
+import org.wfmc._2009.xpdl2.Documentation;
 import org.wfmc._2009.xpdl2.Event;
 import org.wfmc._2009.xpdl2.MessageType;
+import org.wfmc._2009.xpdl2.NodeGraphicsInfo;
+import org.wfmc._2009.xpdl2.NodeGraphicsInfos;
 import org.wfmc._2009.xpdl2.StartEvent;
 import org.wfmc._2009.xpdl2.TriggerResultMessage;
 import org.wfmc._2009.xpdl2.TriggerTimer;
@@ -36,8 +40,6 @@ public class TestEvents {
         StartEvent startE = new StartEvent();
         startE.setTrigger("None");
 
-        // startE.setTriggerTimer(trigger);
-
         //Evento
         Event event = new Event();
         //agrego startEvent al event
@@ -47,9 +49,30 @@ public class TestEvents {
         Description desc = new Description();
         desc.setValue("descripcionn");
 
+        //Documentation
+        Documentation doc = new Documentation();
+
+        //Coordinates
+        Coordinates coord = new Coordinates();
+        coord.setXCoordinate(2.1);
+        coord.setYCoordinate(3.2);
+
+        //NodeGraphicsInfo
+        NodeGraphicsInfo node = new NodeGraphicsInfo();
+        node.setHeight(5.0);
+        node.setWidth(6.0);
+        node.setLaneId("BizAgi_Process_Modeler");
+        node.setCoordinates(coord);
+
+        //NodeGraphicsInfos
+        NodeGraphicsInfos nodeGraph = new NodeGraphicsInfos();
+        nodeGraph.getNodeGraphicsInfo().add(node);
+
         //agrego evento y la descr al content de activity
-        act.getContent().add(act);
         act.getContent().add(desc);
+        act.getContent().add(event);
+        act.getContent().add(doc);
+        act.getContent().add(nodeGraph);
 
         //creo la lista de padres 
         List<Object> pathFromRoot = new ArrayList<Object>();
@@ -68,6 +91,11 @@ public class TestEvents {
         assertEquals("95b04499-8a9e-447c-82b9-b244167bad35", event2.getId());
         assertEquals("actividadPrueba", event2.getName());
         assertEquals("descripcionn", event2.getDescription());
+        assertEquals(2.1, event2.getCoordinate().getX(), 0);
+        assertEquals(3.2, event2.getCoordinate().getY(), 0);
+        assertEquals(6.0, event2.getWidth(), 0);
+        assertEquals(5.0, event2.getHeight(), 0);
+        assertEquals("BizAgi_Process_Modeler", event2.getLane());
     }
 
     @Test
@@ -98,9 +126,30 @@ public class TestEvents {
         Description desc = new Description();
         desc.setValue("descripcionn");
 
-        //agrego evento y la descripcion al content de activity
-        act.getContent().add(act);
+        //Documentation
+        Documentation doc = new Documentation();
+
+        //Coordinates
+        Coordinates coord = new Coordinates();
+        coord.setXCoordinate(2.1);
+        coord.setYCoordinate(3.2);
+
+        //NodeGraphicsInfo
+        NodeGraphicsInfo node = new NodeGraphicsInfo();
+        node.setHeight(5.0);
+        node.setWidth(6.0);
+        node.setLaneId("BizAgi_Process_Modeler");
+        node.setCoordinates(coord);
+
+        //NodeGraphicsInfos
+        NodeGraphicsInfos nodeGraph = new NodeGraphicsInfos();
+        nodeGraph.getNodeGraphicsInfo().add(node);
+
+        //agrego evento y la descr al content de activity
         act.getContent().add(desc);
+        act.getContent().add(event);
+        act.getContent().add(doc);
+        act.getContent().add(nodeGraph);
 
         //creo la lista de padres 
         List<Object> pathFromRoot = new ArrayList<Object>();
@@ -122,7 +171,11 @@ public class TestEvents {
         assertEquals("Timer", event2.getTrigger());
         assertEquals("2013-09-210T00:00:00", event2.getTriggerAttr());
         assertEquals("TimeDate", event2.getTriggeType());
-
+        assertEquals(2.1, event2.getCoordinate().getX(), 0);
+        assertEquals(3.2, event2.getCoordinate().getY(), 0);
+        assertEquals(6.0, event2.getWidth(), 0);
+        assertEquals(5.0, event2.getHeight(), 0);
+        assertEquals("BizAgi_Process_Modeler", event2.getLane());
     }
 
     @Test
@@ -153,9 +206,30 @@ public class TestEvents {
         Description desc = new Description();
         desc.setValue("descripcionn");
 
-        //agrego evento y la descripcion al content de activity
-        act.getContent().add(act);
+        //Documentation
+        Documentation doc = new Documentation();
+
+        //Coordinates
+        Coordinates coord = new Coordinates();
+        coord.setXCoordinate(2.1);
+        coord.setYCoordinate(3.2);
+
+        //NodeGraphicsInfo
+        NodeGraphicsInfo node = new NodeGraphicsInfo();
+        node.setHeight(5.0);
+        node.setWidth(6.0);
+        node.setLaneId("BizAgi_Process_Modeler");
+        node.setCoordinates(coord);
+
+        //NodeGraphicsInfos
+        NodeGraphicsInfos nodeGraph = new NodeGraphicsInfos();
+        nodeGraph.getNodeGraphicsInfo().add(node);
+
+        //agrego evento y la descr al content de activity
         act.getContent().add(desc);
+        act.getContent().add(event);
+        act.getContent().add(doc);
+        act.getContent().add(nodeGraph);
 
         //creo la lista de padres 
         List<Object> pathFromRoot = new ArrayList<Object>();
@@ -177,6 +251,11 @@ public class TestEvents {
         assertEquals("Timer", event2.getTrigger());
         assertEquals("R1/PT10M", event2.getTriggerAttr());
         assertEquals("TimeCycle", event2.getTriggeType());
+        assertEquals(2.1, event2.getCoordinate().getX(), 0);
+        assertEquals(3.2, event2.getCoordinate().getY(), 0);
+        assertEquals(6.0, event2.getWidth(), 0);
+        assertEquals(5.0, event2.getHeight(), 0);
+        assertEquals("BizAgi_Process_Modeler", event2.getLane());
     }
 
     @Test
@@ -214,9 +293,30 @@ public class TestEvents {
         Description desc = new Description();
         desc.setValue("descripcionn");
 
-        //agrego evento y la descripcion al content de activity
-        act.getContent().add(act);
+        //Documentation
+        Documentation doc = new Documentation();
+
+        //Coordinates
+        Coordinates coord = new Coordinates();
+        coord.setXCoordinate(2.1);
+        coord.setYCoordinate(3.2);
+
+        //NodeGraphicsInfo
+        NodeGraphicsInfo node = new NodeGraphicsInfo();
+        node.setHeight(5.0);
+        node.setWidth(6.0);
+        node.setLaneId("BizAgi_Process_Modeler");
+        node.setCoordinates(coord);
+
+        //NodeGraphicsInfos
+        NodeGraphicsInfos nodeGraph = new NodeGraphicsInfos();
+        nodeGraph.getNodeGraphicsInfo().add(node);
+
+        //agrego evento y la descr al content de activity
         act.getContent().add(desc);
+        act.getContent().add(event);
+        act.getContent().add(doc);
+        act.getContent().add(nodeGraph);
 
         //creo la lista de padres 
         List<Object> pathFromRoot = new ArrayList<Object>();
@@ -237,5 +337,10 @@ public class TestEvents {
         assertEquals("descripcionn", event2.getDescription());
         assertEquals("Message", event2.getTrigger());
         assertEquals("b131b0f3-ae5e-40ec-8677-98ee58608498", event2.getMessageId());
+        assertEquals(2.1, event2.getCoordinate().getX(), 0);
+        assertEquals(3.2, event2.getCoordinate().getY(), 0);
+        assertEquals(6.0, event2.getWidth(), 0);
+        assertEquals(5.0, event2.getHeight(), 0);
+        assertEquals("BizAgi_Process_Modeler", event2.getLane());
     }
 }
