877fddbec65a85c8925ebb66c8f78652c35a00e3
tam-09 <tam.th09@gmail.com>
2013-09-28 00:08:32 -0300

    agregado de funciones auxiliares

--- BizativitiProject/src/com/fing/pis/bizativiti/plugin/xpdl/Util.java
+++ BizativitiProject/src/com/fing/pis/bizativiti/plugin/xpdl/Util.java
@@ -1,5 +1,9 @@
 package com.fing.pis.bizativiti.plugin.xpdl;
 
+import org.wfmc._2009.xpdl2.StartEvent;
+import org.wfmc._2009.xpdl2.TriggerResultMessage;
+import org.wfmc._2009.xpdl2.TriggerTimer;
+
 /**
  * Clase con métodos auxiliares utilizados por los translators
  */
@@ -22,6 +26,47 @@ public class Util {
 
     public static String getName(org.wfmc._2009.xpdl2.Activity activity) {
         return activity.getName();
+    }
+
+    //------
+
+    public static String getTriggerAttrTimer(StartEvent event) {
+        TriggerTimer trigger = event.getTriggerTimer();
+        String sTriggerAttr = null;
+        if (trigger.getTimeDateAttr() != null) {
+            sTriggerAttr = trigger.getTimeDateAttr();
+        } else if (trigger.getTimeCycleAttr() != null) {
+            sTriggerAttr = trigger.getTimeCycleAttr();
+        } else {
+            throw new RuntimeException("No attributes inside TriggerTimer tag");
+        }
+        return sTriggerAttr;
+    }
+
+    public static String getTriggerType(StartEvent event) {
+        TriggerTimer trigger = event.getTriggerTimer();
+        String sTriggerType = null;
+        if (trigger.getTimeDateAttr() != null) {
+            sTriggerType = "TimeDate";
+        } else if (trigger.getTimeCycleAttr() != null) {
+            sTriggerType = "TimeCycle";
+        } else {
+            throw new RuntimeException("No attributes inside TriggerTimer tag");
+        }
+        return sTriggerType;
+    }
+
+    public static String getMessageId(StartEvent event) {
+        TriggerResultMessage triggerResult = event.getTriggerResultMessage();
+        if (triggerResult != null) {
+            if (triggerResult.getMessage() != null) {
+                return triggerResult.getMessage().getId();
+            } else {
+                throw new RuntimeException("No attributes inside Message tag");
+            }
+        } else {
+            throw new RuntimeException("No attributes inside TriggerResultMessage tag");
+        }
     }
 
 }
