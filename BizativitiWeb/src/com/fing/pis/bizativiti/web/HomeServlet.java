ab8718a51edc52c110a80b0f64ac221127a9c97b
varmandugon <victoria.armandugon@gmail.com>
2013-10-03 23:00:50 -0300

    COrrecciones a los servlets

--- BizativitiWeb/src/com/fing/pis/bizativiti/web/HomeServlet.java
+++ BizativitiWeb/src/com/fing/pis/bizativiti/web/HomeServlet.java
@@ -1,7 +1,6 @@
 package com.fing.pis.bizativiti.web;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +12,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/")
 public class HomeServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 1L;
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -23,22 +22,13 @@ public class HomeServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+	}
 
-    }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-            IOException {
-        // TODO Auto-generated method stub
-    }
 
 }
