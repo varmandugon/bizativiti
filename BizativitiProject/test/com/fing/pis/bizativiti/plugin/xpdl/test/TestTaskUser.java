4a66a0db2323e2b621364c865e5e6d32a16956ce
“Santiago” <santimoren@gmail.com>
2013-10-04 18:37:04 -0300

    Completa test Tarea de Usuario

--- BizativitiProject/test/com/fing/pis/bizativiti/plugin/xpdl/test/TestTaskUser.java
+++ BizativitiProject/test/com/fing/pis/bizativiti/plugin/xpdl/test/TestTaskUser.java
@@ -14,6 +14,7 @@ import org.wfmc._2009.xpdl2.Task;
 import org.wfmc._2009.xpdl2.TaskUser;
 
 import com.fing.pis.bizativiti.common.metamodel.MetamodelElement;
+import com.fing.pis.bizativiti.common.metamodel.MetamodelUserTask;
 import com.fing.pis.bizativiti.plugin.xpdl.Converter;
 import com.fing.pis.bizativiti.plugin.xpdl.DummyTranslator;
 import com.fing.pis.bizativiti.plugin.xpdl.TranslatorActivity;
@@ -42,12 +43,12 @@ public class TestTaskUser {
         impl.setTask(task);
         content.add(impl);
 
-        coord.setXCoordinate(2.1);
-        coord.setYCoordinate(3.2);
+        coord.setXCoordinate(3.14);
+        coord.setYCoordinate(2.37);
 
         node.setHeight(5.0);
         node.setWidth(6.0);
-        node.setLaneId("BizAgi_Process_Modeler");
+        node.setLaneId("Lane");
         node.setCoordinates(coord);
 
         nodeGraph.getNodeGraphicsInfo().add(node);
@@ -61,6 +62,18 @@ public class TestTaskUser {
         List<MetamodelElement> items = converter.start(act);
 
         assertEquals(1, items.size());
+        assertEquals(true, items.get(0) instanceof MetamodelUserTask);
+        MetamodelUserTask me = (MetamodelUserTask) items.get(0);
+        assertEquals("11111111-1111-1111-1111-111111111111", me.getId());
+        assertEquals("actividadUno", me.getName());
+        assertEquals("", me.getDescription());
+
+        assertEquals("Lane", me.getLane());
+
+        assertEquals(5.0, me.getHeight(), 0);
+        assertEquals(6.0, me.getWidth(), 0);
+        assertEquals(3.14, me.getCoordinate().getX(), 0);
+        assertEquals(2.37, me.getCoordinate().getY(), 0);
 
     }
 }
