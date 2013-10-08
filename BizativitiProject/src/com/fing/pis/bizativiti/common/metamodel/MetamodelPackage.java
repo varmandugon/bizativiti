eab88cf5d5d87445e5e016d1a62ee80875ae30f9
jmpc <jmpc@rain>
2013-10-07 23:42:39 -0200

    Arreglos en el MetamodelPackage

--- BizativitiProject/src/com/fing/pis/bizativiti/common/metamodel/MetamodelPackage.java
+++ BizativitiProject/src/com/fing/pis/bizativiti/common/metamodel/MetamodelPackage.java
@@ -3,38 +3,30 @@ package com.fing.pis.bizativiti.common.metamodel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MetamodelPackage {
+public class MetamodelPackage extends MetamodelElement {
 
     private List<MetamodelElement> elements = new ArrayList<MetamodelElement>();
     private String name = "";
-    private boolean isExecutable; // No se donde se mapea esto en xpdl
 
-    /** Devuelve una copia de la lista de elementos en el paquete */
+    public MetamodelPackage(String id, String description, String name, List<MetamodelElement> elements) {
+        super(id, description);
+        if (name == null) {
+            throw new NullPointerException("name can't be null");
+        }
+        this.name = name;
+
+        if (elements == null) {
+            throw new NullPointerException("elements can't be null");
+        }
+        this.elements.addAll(elements);
+    }
+
     public List<MetamodelElement> getElements() {
         return new ArrayList<MetamodelElement>(elements);
     }
 
-    public void addElement(MetamodelElement element) {
-        if (elements.contains(element)) {
-            throw new IllegalArgumentException("Element " + element + " already exists in the package");
-        }
-        elements.add(element);
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean getIsExecutable() {
-        return isExecutable;
-    }
-
-    public void setIsExecutable(boolean isExecutable) {
-        this.isExecutable = isExecutable;
     }
 
 }
